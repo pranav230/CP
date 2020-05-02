8342578f4d0d7f9b74431d99ee42b36af6d788f8
Pranav <pranavbansal777@gmail.com>
2020-05-02 17:14:05 +0530

    Updated the template

---
+++ Template/README.md
@@ -0,0 +1,3 @@
+# My C++ Template for CP
+
+This folder contains the template I use for competitive Programming
--- Template/template.cpp
+++ Template/template.cpp
@@ -1,30 +1,31 @@
 #include<bits/stdc++.h>
 
+#define int long long
 #define fast ios_base::sync_with_stdio(false); cin.tie(0);
 #define rep(i,a,n) for(int i=a;i<n;i++)
 #define all(x) x.begin(),x.end()
-#define watch(x) cout<<#x<<" = "<<x<<endl;
-#define V(x) vector<x>
-#define P(x,y) pair<x,y>
+#define vi vector<int>
+#define pii pair<int,int>
 #define UMP(x,y) unordered_map<x,y>
 #define MP(x,y) map<x,y>
 #define pb push_back
 #define mp make_pair
 #define fi first
 #define se second
+#define inf 1e18
 #define endl "\n"
 #define coutd(n) cout << fixed << setprecision(n)
-typedef long long ll;
+#define watch(x) cout<<#x<<" = "<<x<<endl;
 typedef double db;
-typedef long double ld;
-const ll mod = 1000000007;
+typedef long double ld;	
+const int mod = 1000000007;
 
 using namespace std;
 
-int main(){
+int32_t main(){
     fast
-    
-	int t;
+    	
+    int t;
 	cin>>t;
 	rep(_,0,t){
 		
